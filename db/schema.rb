eaee6adffcd689f5933290690308132ce408185c
n4r1tahhhh <ryutaro5126@fuji.waseda.jp>
2020-12-23 07:55:52 +0000

    [Add] Event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_074536) do
+ActiveRecord::Schema.define(version: 2020_12_23_075232) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
