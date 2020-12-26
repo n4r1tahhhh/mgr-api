5c0357418e47095ee7f0b1d6a916600055a9e27a
n4r1tahhhh <ryutaro5126@fuji.waseda.jp>
2020-12-26 13:57:10 +0000

    [Add] UsersController(index create show update destroy)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,47 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :update, :destroy]
+
     def index
+        users = User.all
+        render json: users
     end
 
     def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: { "message": "something went wrong" }
+        end
     end
 
     def show
+        render json: @user
     end
 
     def update
+        if @user.update(user_params)
+            render json: @user
+        else
+            render json: { "message": "something went wrong" }
+        end
     end
 
     def destroy
+        if @user.destroy
+            render json: { "message": "User successfully removed!" }
+        else
+            render json: { "message": "something went wrong" }
+        end
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id].to_i)
+    end
+
+    def user_params
+        params.permit(:name)
     end
 end
