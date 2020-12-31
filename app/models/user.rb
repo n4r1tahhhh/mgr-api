5cf63d7e342c1b086eb44ee49e6f24e74e8aa656
n4r1tahhhh <ryutaro5126@fuji.waseda.jp>
2020-12-31 03:43:36 +0000

    [Add] user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_save :downcase_email
+
   validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_many :ownerships
   has_many :submissions
+
+  has_secure_password
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
